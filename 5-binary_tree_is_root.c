b3cc832b04e0d09c0bd04d75ab8754356ce382ba
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-10 00:02:30 +0100

    task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: pointer to the node to check
+ * Return: 1 if a root
+ *         0 if otherwise or NULL
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || node->parent)
+		return (0);
+	return (1);
+}
