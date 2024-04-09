a1182a14783774f1bc8c95fb625a2d3c5a5a0ffc
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-09 17:24:58 +0100

    null parent considered

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node;
 
+	if (!parent)
+		return (NULL);
 	left_node = binary_tree_node(parent, value);
-	if (!left_node || !parent)
+	if (left_node == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
