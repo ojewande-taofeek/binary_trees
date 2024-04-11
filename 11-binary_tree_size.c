11178a35ae52163460bdab7049a39f309bb4a413
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-11 22:27:28 +0100

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a node in a binary tree
+ * @tree: pointer to the node to measure the size
+ * Return: size, total number of nodes
+ *         if tree is NULL, 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t s_left = 0, s_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		s_left = binary_tree_size(tree->left);
+	if (tree->right)
+		s_right = binary_tree_size(tree->right);
+
+	return (s_right + s_left + 1);
+}
