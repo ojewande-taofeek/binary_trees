284bb89515c94b355670fc63c5e3f46f31cf3177
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-11 23:52:18 +0100

    reverted

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,11 +9,9 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent = node->parent->parent;
-
-	if (!node || !(node->parent) || !(grandparent))
+	if (!node || !(node->parent) || !(node->parent->parent))
 		return (NULL);
-	if (node->parent == (grandparent->right))
-		return (grandparent->left);
-	return (grandparent->right);
+	if (node->parent == (node->parent->parent->right))
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
 }
